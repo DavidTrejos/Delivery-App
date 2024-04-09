d4bda59b003b40fc485958789615c515499df725
David Trejos <davidtrejoscortes@gmail.com>
2024-04-09 00:10:07 -0500

    Manage to upload images with blobstorage Firebase

--- src/Data/repositories/AuthRepository.tsx
+++ src/Data/repositories/AuthRepository.tsx
@@ -3,6 +3,9 @@ import { User } from '../../Domain/entities/User';
 import { AuthRepository } from '../../Domain/repositories/AuthRepository';
 import { ApiDelivery } from '../sources/remote/api/ApiDelivery';
 import { ResponseApiDelivery} from '../sources/remote/models/ResponseApiDelivery';
+import { ImagePickerAsset } from 'expo-image-picker';
+import mime from 'mime';
+
 
 export class AuthRepositoryImp implements AuthRepository{
 
@@ -10,6 +13,29 @@ export class AuthRepositoryImp implements AuthRepository{
         try {
 
             const response = await  ApiDelivery.post<ResponseApiDelivery>('/users/create',user)
+            return Promise.resolve(response.data);
+
+        } catch (error) {
+
+            let e = (error as AxiosError);
+            console.log('ERROR'+ JSON.stringify(e.response?.data));
+            const apiError: ResponseApiDelivery = JSON.parse(JSON.stringify(e.response?.data));
+            return Promise.resolve(apiError);
+        }
+    }
+    async registerWithImage(user: User, file: ImagePickerAsset): Promise<ResponseApiDelivery> {
+        try {
+            let data = new FormData();
+
+            data.append('user', JSON.stringify(user));
+            
+            data.append('image', {
+                uri: file.uri,
+                type: mime.getType(file.uri) || 'image/jpeg', // Usar 'image/jpeg' como tipo predeterminado si mime.getType(file.uri) es nulo
+                name: file.uri.split('/').pop() || 'image.jpg' // Usar 'image.jpg' como nombre predeterminado si no se puede obtener un nombre del archivo
+            } as any);
+
+            const response = await  ApiDelivery.post<ResponseApiDelivery>('/users/registerWithImage',user)
             return Promise.resolve(response.data);
 
         } catch (error) {
@@ -38,6 +64,8 @@ export class AuthRepositoryImp implements AuthRepository{
             return Promise.resolve(apiError);
         }
     }
+    
+    
 
 
 
